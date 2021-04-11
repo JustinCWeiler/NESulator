1329a13b22835d6138a796a7d63a07f8c7a40bea
Justin Weiler <jweiler@stanford.edu>
2021-04-11 02:08:48 -0500

    write more for mappers

--- include/mappers.h
+++ include/mappers.h
@@ -6,19 +6,18 @@
 #define PRG_CHUNK 0x4000
 #define CHR_CHUNK 0x2000
 
-typedef uint8_t (*read_func)(uint16_t);
-typedef void (*write_func)(uint16_t, uint8_t);
-
 typedef struct {
-	read_func read;
-	write_func write;
-
-	uint8_t prg_size;
-	uint8_t chr_size;
+	uint8_t num;
 	void* prg_rom;
+	uint8_t prg_size;
 	void* chr_rom;
+	uint8_t chr_size;
 } mapper_t;
 
-mapper_t* create_mapper(uint8_t num, void* prg_rom, void* chr_rom);
+mapper_t* create_mapper(uint8_t num, void* prg_rom, uint8_t prg_size,
+                                     void* chr_rom, uint8_t chr_size);
+
+uint8_t read(mapper_t* mapper, uint16_t addr);
+void write(mapper_t* mapper, uint16_t addr, uint8_t val);
 
 #endif
