04d7981ba7b46a303c3f6e791e24c7898749c880
Justin Weiler <jweiler@stanford.edu>
2021-04-14 01:43:46 -0500

    add some boilerplate for mapper 0

--- src/lib/bus.c
+++ src/lib/bus.c
@@ -3,21 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_DONE 0x00
+#define NUM_DONE 0x01
+
+// time for big code
+#include "mappers_00.c"
 
 typedef void (*create_func)(bus_t*);
 typedef uint8_t (*read_func)(bus_t*, uint16_t);
 typedef void (*write_func)(bus_t*, uint16_t, uint8_t);
 
-// array of pointer to create_funcs
-// would make typedef but its complicated
+size_t sizes[] = {
+	ms00
+};
+
 create_func create_funcs[] = {
+	mc00
 };
 
 read_func read_funcs[] = {
+	mr00
 };
 
 write_func write_funcs[] = {
+	mw00
 };
 
 bus_t* bus_create_simple(read_func read, write_func write) {
@@ -30,7 +38,7 @@ bus_t* bus_create_simple(read_func read, write_func write) {
 }
 
 bus_t* bus_create(uint8_t num, void* prg_rom, uint8_t prg_size,
-                                     void* chr_rom, uint8_t chr_size) {
+                  void* chr_rom, uint8_t chr_size) {
 	if (num >= NUM_DONE) {
 		printf("Sorry, this game isn't supported yet\n");
 		return NULL;
@@ -45,6 +53,8 @@ bus_t* bus_create(uint8_t num, void* prg_rom, uint8_t prg_size,
 	bus->prg_size = prg_size;
 	bus->chr_rom = chr_rom;
 	bus->chr_size = chr_size;
+
+	create_funcs[num](bus);
 
 	return bus;
 }
